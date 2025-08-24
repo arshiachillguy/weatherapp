b3b5bdc3ea73836664d96d6a2996fbc8357513be
arshia <chillguyfromspace@gmail.com>
2025-08-24 21:16:33 +0330

    update and add new service to get xml data

--- src/main/java/com/example/demo/weatherService/serviceforweather.java
+++ src/main/java/com/example/demo/weatherService/serviceforweather.java
@@ -1,5 +1,9 @@
 package com.example.demo.weatherService;
 
+import com.example.demo.WeatherDATA.WeatherResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,13 +17,26 @@ public class serviceforweather {
     //یک کلاس از اسپرینگ برای ارسال درخواست‌های HTTP به APIهای خارجی است.
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public String getWeather(String city){
-        //  ادرس زیر ادرس همون سایتی هستت که ازش  api گرفتیم
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apikey ;
+    private final ObjectMapper objectMapper;
 
-        return restTemplate.getForObject(url , String.class);
+    public serviceforweather(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
 
+//    public String getWeather(String city){
+//        //  ادرس زیر ادرس همون سایتی هستت که ازش api گرفتیم
+//        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apikey ;
+//
+//        return restTemplate.getForObject(url , String.class);
+//    }
+
+    public WeatherResponse GETweather(String city) throws JsonProcessingException {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city
+                + "&units=metric&appid=" + apikey;
+
+        String json = restTemplate.getForObject(url, String.class);
+        return objectMapper.readValue(json, WeatherResponse.class);
+    }
 
 
 
