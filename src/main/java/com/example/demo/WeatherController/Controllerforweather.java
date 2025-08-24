e70245895e95f9b63518eed4903f5a935d4bd70e
arshia <chillguyfromspace@gmail.com>
2025-08-24 21:17:27 +0330

    update and add new controller

--- src/main/java/com/example/demo/WeatherController/Controllerforweather.java
+++ src/main/java/com/example/demo/WeatherController/Controllerforweather.java
@@ -1,7 +1,9 @@
 package com.example.demo.WeatherController;
 
+import com.example.demo.WeatherDATA.WeatherResponse;
 import com.example.demo.weatherService.serviceforweather;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/weather") // ادرس پیش فرض در حال حاضر localhost:8080/weather
@@ -11,17 +13,24 @@ public class Controllerforweather {
     // در نسخه های جدید اسپرینگ اگر کلا یک constructor داشته باشه کلاس ما به صورت خودکار میاد اینجا و نیازی به تزریق وابستگی با استفاده از AutoWired@ نیست
     //
     private final serviceforweather serviceforweather;
-
-    public Controllerforweather(serviceforweather serviceforweather) {
+    public Controllerforweather(serviceforweather serviceforweather, WeatherResponse weatherResponse) {
         this.serviceforweather = serviceforweather;
+
     }
     // در صفحه وب ما کنترلر ما به ادرس پیشفرض پیام هارو میفرسته که این ادرس پیشفرض اخرش باید اسم شهر باشه تا این کنترلر کار کنه
     // دلیل استفاده از متد getmapping هم مشخصه ما فقط میخواییم اطلاعات رو بگیریم همین
-    @GetMapping("/{city}")
-    public String GetWeather(@PathVariable String city){
-        return serviceforweather.getWeather(city);
+
+
+//
+//    @GetMapping("/{city}")
+//    public String GetWeather(@PathVariable String city){
+//        return serviceforweather.getWeather(city);
+//    }
+
+
+    @GetMapping(value = "/2/{city}" , produces = {MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE})
+    public WeatherResponse getWeather(@PathVariable String city) throws JsonProcessingException {
+        return serviceforweather.GETweather(city);
     }
-
-
 
 }
