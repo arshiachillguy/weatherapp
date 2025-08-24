4bc2f7e49cd8d873936716a48cdc54582d25eef6
arshia <chillguyfromspace@gmail.com>
2025-08-24 21:11:40 +0330

    update WeatherResponse and add some linse

--- src/main/java/com/example/demo/WeatherDATA/WeatherResponse.java
+++ src/main/java/com/example/demo/WeatherDATA/WeatherResponse.java
@@ -1,43 +1,56 @@
 package com.example.demo.WeatherDATA;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @JacksonXmlRootElement(localName = "WeatherResponse")
 @Component
 public class WeatherResponse {
+
+    @JsonProperty("name")
     @JacksonXmlProperty(localName = "location")
     private String city;
 
-    @JacksonXmlProperty(localName = "Temperature")
-    private double temp;
+    @JsonProperty("main")
+    private Main main;
+
+    @JsonProperty("weather")
+    private List<WeatherData> weather;
+
+
+    @JacksonXmlProperty(localName = "City")
+    public String getCity() {
+        return city != null ? city : "Unknown" ;
+    }
+
+    @JacksonXmlProperty(localName = "temprature")
+    public double getTemprature(){
+        return main != null ? main.temp : 0.0;
+    }
 
     @JacksonXmlProperty(localName = "Condition")
-    private String description;
-
-
-    public String getCity() {
-        return city;
+    public String getCondition() {
+        if (weather != null && !weather.isEmpty()){
+            return weather.get(0).getDescription();
+        }
+        return "No data";
     }
 
-    public void setCity(String city) {
-        this.city = city;
+
+
+
+
+    public static class Main{
+        @JsonProperty("temp")
+        public double temp;
     }
 
-    public double getTemp() {
-        return temp;
-    }
-
-    public void setTemp(double temp) {
-        this.temp = temp;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public static class weather{
+        @JsonProperty("description")
+        public String description;
     }
 }
